f25d476e6e3f88695379ce4974f577cdc8f02717
DarkBear0 <baphometmarx@gmail.com>
2024-06-08 18:19:22 +0200

    miss

--- src/models/TicketPrice.java
+++ src/models/TicketPrice.java
@@ -5,8 +5,6 @@ public class TicketPrice {
     private int cents;
 
     public TicketPrice(int dollars, int cents) {
-        if (dollars < 0 || cents < 0 || cents >= 100)
-            throw new IllegalArgumentException("Invalid price values. Dollars should be non-negative, and cents should be between 0 and 99.");
         this.dollars = dollars;
         this.cents = cents;
     }
